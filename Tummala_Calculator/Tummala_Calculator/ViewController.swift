f53a985f3f38de5235104305065480feee28771a
rushyanth1130 <85264598+rushyanth1130@users.noreply.github.com>
2022-02-14 14:02:49 -0600

    added buttons in view controller

--- Tummala_Calculator/Tummala_Calculator/ViewController.swift
+++ Tummala_Calculator/Tummala_Calculator/ViewController.swift
@@ -12,26 +12,65 @@ class ViewController: UIViewController {
     @IBOutlet weak var ResultLabel: UILabel!
     
     
+    @IBAction func buttonAC(_ sender: UIButton) {
+    }
     
+    @IBAction func buttonC(_ sender: UIButton) {
+    }
     
+    @IBAction func buttonplus_minus(_ sender: UIButton) {
+    }
     
+    @IBAction func buttonDiv(_ sender: UIButton) {
+    }
     
+    @IBAction func button7(_ sender: UIButton) {
+    }
     
+    @IBAction func button8(_ sender: UIButton) {
+    }
     
+    @IBAction func button9(_ sender: UIButton) {
+    }
     
+    @IBAction func buttonX(_ sender: UIButton) {
+    }
     
+    @IBAction func button4(_ sender: UIButton) {
+    }
     
+    @IBAction func button5(_ sender: UIButton) {
+    }
     
+    @IBAction func button6(_ sender: UIButton) {
+    }
     
+    @IBAction func buttonminus(_ sender: UIButton) {
+    }
     
+    @IBAction func button1(_ sender: UIButton) {
+    }
     
+    @IBAction func button2(_ sender: UIButton) {
+    }
     
+    @IBAction func button3(_ sender: UIButton) {
+    }
     
+    @IBAction func buttonplus(_ sender: UIButton) {
+    }
     
+    @IBAction func buttonzero(_ sender: UIButton) {
+    }
     
+    @IBAction func buttondot(_ sender: UIButton) {
+    }
     
+    @IBAction func buttonpercent(_ sender: UIButton) {
+    }
     
-    
+    @IBAction func buttonequals(_ sender: UIButton) {
+    }
     
     
     
